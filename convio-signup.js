ca5e6dd7cd4fed66d90eaa00c6f1d928fc508a42
Joshua Bousel <josh@bousel.com>
2016-05-17 10:05:41 -0400

    Updated logic to allow for more flexibility

--- convio-signup.js
+++ convio-signup.js
@@ -3,84 +3,74 @@ var $ = require('jquery');
 
 $(function(){
 	var $emailContainer = $('.convio-email-signup');
-	var dataFields = $emailContainer.attr('data-fields').toLowerCase();
 	var surveyID = $emailContainer.attr('data-surveyid');
-	var thankYouMessage = $emailContainer.attr('data-thankyou') != '' ? $emailContainer.attr('data-thankyou') : 'Thank you for signing up';
-	var hasFirstName = dataFields.indexOf("first") >= 0 ? true : false;
-	var hasLastName = dataFields.indexOf("last") >= 0 ? true : false;
-	var hasEmail = dataFields.indexOf("email") >= 0 ? true : false;
-	var formHTML;
-	
-	var formHTML = '<div class="form-container">';		
+	var reqs = [];
+	var maps = [];
+	var $thanks = $('.convio-email-signup .thanks');
+	var formHTML = '<div class="form-container">';
 	formHTML += '<style>.convio-email-signup { width: 80%; margin: 0 auto 15px; } .convio-email-signup .form-row { margin: 0 0 25px; } .convio-email-signup .input-full { width: 100%; } .convio-email-signup .input-text { box-shadow: none; border: 1px solid #e8e7e5; transition: border 0.2s ease-out; -webkit-appearance: none; -moz-appearance: none; appearance: none; } .convio-email-signup .input-text:focus { outline: none; border: 1px solid #A2A2A2; } .convio-email-signup .error { background-color: #cd4e38; border-radius: 4px; padding: 3px 15px; color: #fff; margin: 0 0 15px; } .convio-email-signup .hidden { visibility: hidden; opacity: 0; height: 0; } .convio-email-signup .input-error { border: 1px solid #cd4e38; } .convio-email-signup div, .convio-email-signup .error:not(.hidden) { opacity: 1; transition: opacity 0.2s ease-out; } .convio-email-signup .form-thanks { text-align: center; } </style>';
-	
-	formHTML += '<p class="error hidden">Hello</p>';
-	
-	if (hasFirstName) {
-		formHTML += '<div class="form-row"><label class="color-primary type-small" for="first">First Name</label><input type="text" class="input-text input-full" name="first" id="first"></div>';
-	}
-	if (hasLastName) {
-		formHTML += '<div class="form-row"><label class="color-primary type-small" for="last">Last Name</label><input type="text" class="input-text input-full" name="last" id="last"></div>';
-	}
-	if (hasEmail) {
-		formHTML += '<div class="form-row"><label class="color-primary type-small" for="email">Email</label><input type="text" class="input-text input-full" name="email" id="email"></div>';
-	}
+	formHTML += '<p class="error hidden"></p>';
+
+	$('.convio-email-signup input').each(function(){
+		var $input = $(this);
+		var id = $input.attr('data-id');
+		reqs.push($input.attr('data-req'));
+		maps.push($input.attr('data-map'));
+		
+		formHTML += '<div class="form-row"><label class="color-primary type-small" for="'+id+'">'+$input.attr('data-label')+'</label><input type="'+$input.attr('type')+'" class="input-text input-full" name="'+id+'" id="'+id+'"></div>';
+		
+	});
 	
 	formHTML += '<a href="#" class="btn-large fill-callout inline-block">Submit</a>';
 	formHTML += '</div>';
-	formHTML += '<div class="form-thanks hidden">'+thankYouMessage+'</div>';
-	
-	$emailContainer.html(formHTML);
+	formHTML += '<div class="form-thanks hidden">'+$thanks.html()+'</div>';
+	$emailContainer.html(formHTML).show();
 	
 	$('.convio-email-signup .btn-large').on('click',function(e){
 		e.preventDefault();
-		
-		var $first = $('.convio-email-signup #first');
-		var $last = $('.convio-email-signup #last');
+
+		var vals = [];
 		var $email = $('.convio-email-signup #email');
 		var errorClass = 'input-error';
 		var errorBlock = $('.convio-email-signup .error');
 		var errorCount = 0;
+		var totalInputs = 0;
+		var offset = $('.convio-email-signup').offset();
 		
-		$first.removeClass(errorClass);
-		$last.removeClass(errorClass);
-		$email.removeClass(errorClass);
+		$('.convio-email-signup input').each(function(i){
+			var $input = $(this);
+			var val = $input.val();
+			vals.push(val);
+			$input.removeClass(errorClass);
+			totalInputs++;
+			
+			if (reqs[i] == 'true' && val == '') {
+				$input.addClass(errorClass);
+				errorCount++;
+			}
+			
+		});
 		
-		if (hasFirstName && $first.val() == '') {
-			$first.addClass(errorClass);
-			errorCount++;
-		}
-		if (hasLastName && $last.val() == '') {
-			$last.addClass(errorClass);
-			errorCount++;
-		}
-		if (hasEmail && $email.val() == '') {
-			$email.addClass(errorClass);
-			errorCount++;
-		}
 		if (errorCount != 0) {
 			errorBlock.removeClass('hidden');
 			errorBlock.html('Please complete the following fields:');
+			$('html, body').animate({ scrollTop: offset.top }, 250);
 		} else {
-			if (hasEmail && !isValidEmail($email.val())) {
+			if ($email.length && !isValidEmail($email.val())) {
 				$email.addClass(errorClass);
 				errorBlock.removeClass('hidden');
 				errorBlock.html('That email address is not valid!');
+				$('html, body').animate({ scrollTop: offset.top }, 250);
 			} else {
 				var url = 'http://e.wcs.org/site/Survey?cons_info_component=t&SURVEY_ID='+surveyID+'&ACTION_SUBMIT_SURVEY_RESPONSE=Submit';	
-				
-				if (hasFirstName) {
-					url += '&cons_first_name='+$first.val();
-				}
-				if (hasLastName) {
-					url += '&cons_last_name='+$last.val();
-				}
-				if (hasEmail) {
-					url += '&cons_email='+$email.val();
+								
+				for (var i = 0; i < totalInputs; i++) {
+					url += '&'+maps[i]+'='+vals[i];
 				}
 							
-				url = encodeURI(url)
+				url = encodeURI(url);
 				url = url.replace('#','%23');
+				console.log(url);
 				
 				$.ajax({
 					type: "POST",
@@ -88,6 +78,7 @@ $(function(){
 				}).always(function(){
 					$('.convio-email-signup .form-thanks').removeClass('hidden');
 					$('.convio-email-signup .form-container').addClass('hidden');
+					$('html, body').animate({ scrollTop: offset.top }, 250);
 				});
 			}
 		}
